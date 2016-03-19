1cda1cbd06b62c558261a35303ab129fe150a93f
seedante <seedante@gmail.com>
2016-03-19 15:14:47 +0800

    update comments in code

--- ScrollTabBarController/ScrollTabBarController/SlideAnimationController.swift
+++ ScrollTabBarController/ScrollTabBarController/SlideAnimationController.swift
@@ -100,7 +100,21 @@ class SlideAnimationController: NSObject, UIViewControllerAnimatedTransitioning 
         })
     }
 
-//    //使用 Core Animation 的话，交互中止时动画直接跳转到最终状态，应该使用 UIView 动画。
+    /*
+    下面这段被注释的代码是使用 Core Animation 来实现转场，在非交互转场时没有问题，在交互转场下会有点瑕疵，要达到完美要麻烦一点。
+    
+    交互转场必须用 UIView Animtion API 才能实现完美的控制，其实并不是 Core Animation 做不到，
+    毕竟 UIView Animation 是基于 Core Animation 的，那为什么苹果的工程师在 WWDC 上说必须使用前者呢。
+    因为使用 Core Animation 来实现成本高啊，在转场中做到与 UIView Animation 同样的事情配置麻烦些，估计很多人都不会配置，
+    而且在交互转场中会比较麻烦，本来转场 API 已经分裂得够复杂了，老老实实用高级 API 吧。
+    
+    上面说的 UIView Animation API 指的是带 completion 闭包的 API，使用 Core Animation 来实现这个闭包需要配置
+    CATransaction，这就是麻烦的地方，还是用高级的 API 吧。
+    
+    在自定义的容器控制器转场中，交互部分需要我们自己动手实现控制动画的进度，此时 使用 Core Animation 或 UIView Animation
+    区别不大，重点在于在手势中如何控制动画。
+    */
+
 //    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
 //        guard let containerView = transitionContext.containerView(), fromVC = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey), toVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) else{
 //            return
